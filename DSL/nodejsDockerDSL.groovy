c350538773eecca794355c93d1c99845597c9878
alvaro2042 <128509813+alvaro2042@users.noreply.github.com>
2023-03-24 11:22:55 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,21 +1,21 @@
-job('Aplicacion Node.js Docker DSL') {
+job('Aplicacion NodeJS Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/alvaro2042/nodejsapp.git', 'main') { node ->
+            node / gitConfigName('alvaro2042')
+            node / gitConfigEmail('acamargo@lucasian.com')
         }
     }
     triggers {
-        scm('H/7 * * * *')
+        scm('H/1 * * * *')
     }
     wrappers {
         nodejs('nodejs')
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
-            tag('${GIT_REVISION,length=7}')
+            repositoryName('alvaro2042/nodejsapp.git')
+            tag('${GIT_REVISION,length=1}')
             registryCredentials('docker-hub')
             forcePull(false)
             createFingerprints(false)
